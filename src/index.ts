6256d8e4f278e07685f734afe6f298d4eee8e8fb
Minh Nguyen <minh.nguyen@enouvo.com>
2024-11-23 14:03:31 +0700

    init prj

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env.PORT;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(port, () => {
+  console.log("Server listening on port ", port);
+});
