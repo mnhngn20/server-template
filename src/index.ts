a5d16e3dcc725a4c1a1a5eebb876969f162ac02b
Minh Nguyen <minh.nguyen@enouvo.com>
2024-12-07 17:24:30 +0700

    adjust secrets variables

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,7 @@ async function initExpressApp() {
 
   app.get("/", (req, res) => {
     console.log("i was called: dit con me....?");
-    res.send("hello world");
+    res.send("hello world 2");
   });
 
   return app;
